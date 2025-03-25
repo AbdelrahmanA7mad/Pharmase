4164dd126673c52fe2dd15177347be87048298af
GrayCodee <msaa820@gmail.com>
2025-03-25 02:44:39 +0200

    add

--- Pharmase/Models/Category.cs
+++ Pharmase/Models/Category.cs
@@ -1,11 +1,1 @@
-﻿namespace Pharmase.Models
-{
-    public class Category
-    {
-        // hello
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Medicine> Medicines { get; set; }
-    }
-
-}
+﻿
