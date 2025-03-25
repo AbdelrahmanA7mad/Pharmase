75961b79a3601cb1918acd92c9e5705ebc9982cc
GrayCodee <msaa820@gmail.com>
2025-03-25 02:32:04 +0200

    Add project files.

--- Pharmase/Models/Category.cs
+++ Pharmase/Models/Category.cs
@@ -2,6 +2,7 @@
 {
     public class Category
     {
+        // hello
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Medicine> Medicines { get; set; }
